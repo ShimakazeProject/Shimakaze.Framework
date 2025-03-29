38b00225fbd1a5ae4706e987fcb892338f6a284a
舰队的偶像-岛风酱! <frg2089@outlook.com>
2025-03-29 09:24:56 +0800

    update
    Signed-off-by: 舰队的偶像-岛风酱! <frg2089@outlook.com>

--- src/kernel/Kernel/Controls/ChildrenUIElement.cs
+++ src/kernel/Kernel/Controls/ChildrenUIElement.cs
@@ -1,6 +1,42 @@
+using System.Collections.Specialized;
+using System.Diagnostics;
+
 namespace Shimakaze.Framework.Controls;
 
-public abstract class ChildrenUIElement : UIElement
+public abstract class ChildrenUIElement : UIElement, INotifyCollectionChanged
 {
-    public List<UIElement>? Children { get; init; } = [];
+    private readonly List<UIElement> _children = [];
+
+    public IReadOnlyList<UIElement> Children => _children.AsReadOnly();
+
+    public event NotifyCollectionChangedEventHandler? CollectionChanged;
+
+    public void Add(UIElement child)
+    {
+        if (_children.Contains(child))
+            return;
+
+        Debug.Assert(child is { Parent: null });
+        if (child is not { Parent: null })
+            throw new InvalidOperationException();
+
+        _children.Add(child);
+        child.Parent = this;
+        OnCollectionChanged(NotifyCollectionChangedAction.Add, child);
+    }
+
+    public void Remove(UIElement child)
+    {
+        if (!_children.Contains(child))
+            return;
+
+        child.Parent = null;
+        _children.Remove(child);
+        OnCollectionChanged(NotifyCollectionChangedAction.Remove, child);
+    }
+
+    protected virtual void OnCollectionChanged(NotifyCollectionChangedAction action, UIElement changedItem)
+    {
+        CollectionChanged?.Invoke(this, new(action, changedItem));
+    }
 }
--- src/kernel/Kernel/Controls/ContentUIElement.cs
+++ src/kernel/Kernel/Controls/ContentUIElement.cs
@@ -1,3 +1,5 @@
+using System.Diagnostics;
+
 namespace Shimakaze.Framework.Controls;
 
 public abstract partial class ContentUIElement : UIElement
@@ -10,12 +12,19 @@ public abstract partial class ContentUIElement : UIElement
         {
             if (field == value)
                 return;
+
+            Debug.Assert(field is { Parent: null });
+            if (field is not null and not { Parent: null })
+                throw new InvalidOperationException();
+
             var old = field;
             if (field is not null)
                 field.Parent = null;
+
             field = value;
             if (field is not null)
                 field.Parent = this;
+
             OnContentChanged(new(old, value));
         }
     }
